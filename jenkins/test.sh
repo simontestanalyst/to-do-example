cc133cf0b2f2f6e57f9ef7883a3960f70e6b4a6f
psoye <formybills40@gmail.com>
2022-02-22 16:21:38 +0000

    updated test.sh file

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -4,6 +4,6 @@ source venv/bin/activate
 python3 -m pytest \
 	--cov=application \
     --cov-report term-missing \
-    --cov-report xml:coverage.xml \
+    --cov-report=xml:coverage.xml \
     --junitxml=junit_report.xml
     
