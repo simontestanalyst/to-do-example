5ae5f9468a5186892310e14a6ce00f8d559d4b75
psoye <formybills40@gmail.com>
2022-02-22 14:52:50 +0000

    simon-o:added db schema set up, run app and run tests scripts

---
+++ jenkins/test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source venv/bin/activate
+python3 -m pytest \
+	--cov=application \
+    --cov-report term-missing \
+    --cov-report xml:coverage.xml \
+    --junitxml=junit_report.xml
+    
